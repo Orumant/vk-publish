a4d601d8ef1f03963a2674e1bf3ac7f73f1e14a3
Orumant <asp.w@inbox.ru>
2019-09-25 13:14:12 +0300

    Updates

---
+++ precache-manifest.e64bbc3beda56b7d2e3a40627568db2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21955e56b63f94d42c669c00c715f913",
+    "url": "/vk-publish/index.html"
+  },
+  {
+    "revision": "075cc7d6761c7c056ae7",
+    "url": "/vk-publish/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "ceeb71ae86efee210f2b",
+    "url": "/vk-publish/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "075cc7d6761c7c056ae7",
+    "url": "/vk-publish/static/js/2.864cacc6.chunk.js"
+  },
+  {
+    "revision": "ceeb71ae86efee210f2b",
+    "url": "/vk-publish/static/js/main.209c47e5.chunk.js"
+  },
+  {
+    "revision": "8f7f9a43233abe248161",
+    "url": "/vk-publish/static/js/runtime-main.b3172513.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-publish/static/media/persik.4e1ec840.png"
+  }
+]);
